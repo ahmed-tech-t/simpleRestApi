52ff07d07e08c941e02f0fea905c82e2a4005de4
ahmed <codo0161606@gmail.com>
2021-12-27 00:19:41 +0300

    init Commit

--- app/src/main/java/com/example/simplerestapi/ApiInterface.java
+++ app/src/main/java/com/example/simplerestapi/ApiInterface.java
@@ -1,11 +1,14 @@
 package com.example.simplerestapi;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface ApiInterface {
 
-    @GET("posts/{id}")
-    public Call<Post> getPost(@Path("id") int postId);
+    @GET("posts")
+    public Call<List<Post>> getPost(@Query("postId") int postId);
 }
--- app/src/main/java/com/example/simplerestapi/MainActivity.java
+++ app/src/main/java/com/example/simplerestapi/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.simplerestapi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -24,14 +27,14 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         ApiInterface apiInterface = retrofit.create(ApiInterface.class);
 
-        Call<Post> call = apiInterface.getPost(2);
-        call.enqueue(new Callback<Post>() {
+        Call<List<Post>> call = apiInterface.getPost(2);
+        call.enqueue(new Callback<List<Post>>() {
             @Override
-            public void onResponse(Call<Post> call, Response<Post> response) {
-                body.setText(response.body().getTitle());
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                body.setText(response.body().get(0).getTitle());
             }
             @Override
-            public void onFailure(Call<Post> call, Throwable t) {
+            public void onFailure(Call<List<Post>> call, Throwable t) {
                 body.setText(t.getMessage());
             }
         });
